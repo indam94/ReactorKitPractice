e0071bba2c2203b36f5a09267ee760e4a205e737
indam <d0n99un17@gmail.com>
2021-05-12 15:24:25 +0900

    setContent in RootVC

--- ReactorKitPractice/Sources/ViewControllers/RootViewController.swift
+++ ReactorKitPractice/Sources/ViewControllers/RootViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxSwift
+import SnapKit
 
 final class RootViewController: UIViewController{
     
@@ -17,5 +18,43 @@ final class RootViewController: UIViewController{
         super.viewDidLoad()
         
         print("View Did Load \(self.description)")
+        
+        if let contentViewController = contentViewController{
+            contentViewController.view.snp.makeConstraints{
+                $0.edges.equalToSuperview()
+            }
+        }
+    }
+    
+    func setContent(_ viewController: UIViewController?, animated: Bool){
+        let previousViewController = contentViewController
+        
+        if let viewController = viewController{
+            addChild(viewController)
+            view.addSubview(viewController.view)
+//            view.sendSubviewToBack(viewController.view)
+            contentViewController = viewController
+            
+            if isViewLoaded{
+                viewController.view.snp.makeConstraints{
+                    $0.edges.equalToSuperview()
+                }
+            }
+        }
+        
+        if animated{
+            UIView.animate(withDuration: 0.125, delay: 0, options: [.curveLinear], animations: {
+                previousViewController?.view.alpha = 0
+            }){ (success) in
+                previousViewController?.dismiss(animated: false)
+                previousViewController?.view.removeFromSuperview()
+                previousViewController?.removeFromParent()
+            }
+        }
+        else{
+            previousViewController?.dismiss(animated: false)
+            previousViewController?.view.removeFromSuperview()
+            previousViewController?.removeFromParent()
+        }
     }
 }
