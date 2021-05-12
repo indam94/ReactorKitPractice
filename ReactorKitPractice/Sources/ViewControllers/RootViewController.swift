897a312bb555a09bbe947ee11ebb5fe6c4662518
Tong <tong@hpcnt.com>
2021-05-12 12:52:59 +0900

    add RootViewControlelr

---
+++ ReactorKitPractice/Sources/ViewControllers/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  ReactorKitPractice
+//
+//  Created by Tong on 2021/05/12.
+//
+
+import UIKit
+import RxSwift
+
+final class RootViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("View Did Load \(self.description)")
+    }
+}
